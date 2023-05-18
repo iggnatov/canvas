d27736c89fbc15d5a16c5bc0f8d3ef3421215530
iggnatov <iggnatov@gmail.com>
2023-05-18 17:07:31 +0300

    Set gentelella alela template

--- urls.py
+++ urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    # Matches any html file - to be used for gentella
+    # Avoid using your .html in your resources.
+    # Or create a separate django app.
+    re_path(r'^.*\.html', views.gentella_html, name='gentella'),
+
+    # The home page
     path('', views.index, name='index'),
 ]
--- views.py
+++ views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'schooldata/index.html')
+    context = {}
+    template = loader.get_template('schooldata/index.html')
+    return HttpResponse(template.render(context, request))
+
+
+def gentella_html(request):
+    context = {}
+    # The template to be loaded as per gentelella.
+    # All resource paths for gentelella end in .html.
+
+    # Pick out the html file name from the url. And load that template.
+    load_template = request.path.split('/')[-1]
+    template = loader.get_template('schooldata/' + load_template)
+    return HttpResponse(template.render(context, request))
